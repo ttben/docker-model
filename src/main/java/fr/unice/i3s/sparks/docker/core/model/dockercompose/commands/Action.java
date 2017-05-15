694e7e7d7f0bad83e92d4e32ee946982bfc01113
Benjamin Benni <benni@i3s.unice.fr>
2017-05-15 15:42:53 +0200

    Add AddService

---
+++ src/main/java/fr/unice/i3s/sparks/docker/core/model/dockercompose/commands/Action.java
@@ -0,0 +1,4 @@
+package fr.unice.i3s.sparks.docker.core.model.dockercompose.commands;
+
+public class Action {
+}
---
+++ src/main/java/fr/unice/i3s/sparks/docker/core/model/dockercompose/commands/AddService.java
@@ -0,0 +1,8 @@
+package fr.unice.i3s.sparks.docker.core.model.dockercompose.commands;
+
+import fr.unice.i3s.sparks.docker.core.model.dockercompose.DockerCompose;
+
+public class AddService extends Action {
+    public AddService(DockerCompose dockerCompose, String serviceName) {
+    }
+}

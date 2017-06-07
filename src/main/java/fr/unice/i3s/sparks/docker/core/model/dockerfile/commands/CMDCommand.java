9129706f0c8aae52f763cf4ccafe87efafccbc92
Benjamin Benni <benni@i3s.unice.fr>
2017-06-07 10:25:09 +0200

    Update to match req

--- src/main/java/fr/unice/i3s/sparks/docker/core/model/dockerfile/commands/CMDCommand.java
+++ src/main/java/fr/unice/i3s/sparks/docker/core/model/dockerfile/commands/CMDCommand.java
@@ -1,15 +1,28 @@
 package fr.unice.i3s.sparks.docker.core.model.dockerfile.commands;
 
+import fr.unice.i3s.sparks.docker.core.model.dockerfile.Dockerfile;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class CMDCommand extends Command{
 
+    private Dockerfile dockerfile;
     private final List<String> body;
 
-    public CMDCommand(String... body) {
+    public CMDCommand(Dockerfile dockerfile, String... body) {
+        this.dockerfile = dockerfile;
         this.body = new ArrayList<>(Arrays.asList(body));
+    }
+
+    public CMDCommand(String... body) {
+        this.dockerfile = null;
+        this.body = new ArrayList<>(Arrays.asList(body));
+    }
+
+    public Dockerfile getParent() {
+        return this.dockerfile;
     }
 
     public List<String> getBody() {
